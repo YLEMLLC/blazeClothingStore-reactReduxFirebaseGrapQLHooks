68f522a66ff5ab81806c4a50c42b86efe6a1ba51
Ylemm <blitz@Ylem.local>
2020-11-08 16:08:38 -0600

    broken our home page into smaller components, finished styling our menue-item

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { directive } from "@babel/types";
 import React from "react";
-import { HomePage } from "./Homepage.component";
+import { HomePage } from "./pages/homepage/homepage.component";
+import "./App.css";
 
 function App() {
-  return <HomePage></HomePage>;
+  return (
+    <h1>
+      <HomePage />
+    </h1>
+  );
 }
 
 export default App;
