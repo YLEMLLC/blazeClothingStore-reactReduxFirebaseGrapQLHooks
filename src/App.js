476e230f49f46125a2b0ea7298cc3d9ec05bd1b6
Ylemm <blitz@Ylem.local>
2020-11-08 21:02:05 -0600

    Added hats Router, having trouble with exporting menu-item with withRouter. File will not export going to commit and close the entire vsC and reopen to see it it helps

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import { directive } from "@babel/types";
 import React from "react";
-import { HomePage } from "./pages/homepage/homepage.component";
+import { Route } from "react-router-dom";
+
 import "./App.css";
+
+import { HomePage } from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <h1>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route pathe="/hatspage" comonent={HatsPage} />
     </h1>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,19 @@
 import React from "react";
+
+import { withRouter } from "react-router-dom";
 import "./menu-styles.scss";
 
-export const MenuItem = ({ title, imageUrl, size }) => {
-  return (
-    <div className={`${size} menu-item`}>
-      <div
-        className="background-image"
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
-      />
-      <div className="content">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <span className="subtitle">SHOP NOW</span>
-      </div>
+export const MenuItem = ({ title, imageUrl, size, history }) => (
+  <div className={`${size} menu-item`}>
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">SHOP NOW</span>
     </div>
-  );
-};
+  </div>
+);
