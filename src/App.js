646b88fc5bfd9cd3c77c52f2dbba0eef0bd3d7a8
Ylemm <blitz@Ylem.local>
2020-11-10 02:43:20 -0600

    Added sign in page, sign in coponent, input component, custom button component and createda form input compoennt

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 
 import Header from "./components/header/header.component.jsx";
 import HomePage from "./pages/homepage/homepage.component";
+import SignInAndSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
 import ShopPage from "./pages/shop/shoppage.component";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signIn" component={SignInAndSignUpPage} />
       </Switch>
     </h1>
   );
