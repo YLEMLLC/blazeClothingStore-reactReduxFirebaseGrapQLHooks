a5dfa38d19772c1759d311b484ef737d09c65a23
Ylemm <blitz@Ylem.local>
2020-11-18 22:25:59 -0600

    added redux-persist with local storage for our cart.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 
 import rootRucer from './root-reducer';
 
 const middlewres = [logger];
-const store = createStore(rootRucer, applyMiddleware(...middlewres));
 
-export default store;
+export const store = createStore(rootRucer, applyMiddleware(...middlewres));
+
+export const persistor = persistStore(store);
